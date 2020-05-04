57f84ecd85bdd86d353ff4661d76624501b7d1d6
Sanket <sanket16101@iiitnr.edu.in>
2020-05-05 00:17:46 +0100

    Modified settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u_1a4*!!#e21pbhrg=@iw!nn(5*d47zya+lj(v%5^8s71kh+a1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
